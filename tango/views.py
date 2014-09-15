98192516a0a907352c515189fd63ef12db61c0e2
martinkro <martinkro@tencent.com>
2014-09-15 20:35:48 +0800

    tango app

--- tango/views.py
+++ tango/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    #return HttpResponse("Hello index.")
+    context = RequestContext(request)
+    context_dict = {'boldmessage':"I am bold font from the context"}
+    return render_to_response("tango/index.html",context_dict,context)
